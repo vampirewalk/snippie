233f6d7fb24a6018f51f6cf6727f05d1e3fd6de3
Kevin Shen <kevin.shen@fonestock.com>
2013-07-07 17:04:51 +0800

    Import many snippets from https://github.com/mattt/Xcode-Snippets

---
+++ readable_snippets/kvo.m
@@ -0,0 +1,10 @@
+// KVO Setter
+// Use when overriding @synthesized property setter methods
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+[self willChangeValueForKey:@"<#ivar#>"];
+<#ivar#> = <#newValue#>;
+[self didChangeValueForKey:@"<#ivar#>"];
---
+++ readable_snippets/tvdel.m
@@ -0,0 +1,12 @@
+// UITableViewDelegate
+// Placeholders for the core table view delegate methods
+// 
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    <#statements#>
+}
